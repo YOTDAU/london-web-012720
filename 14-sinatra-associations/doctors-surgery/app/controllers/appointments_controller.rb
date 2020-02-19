c8fb53de1214296053a62689450eddba571ba7b1
Sam Barker <samuel.e.barker@googlemail.com>
2020-02-19 10:47:02 +0000

    14 after lecture

--- 14-sinatra-associations/doctors-surgery/app/controllers/appointments_controller.rb
+++ 14-sinatra-associations/doctors-surgery/app/controllers/appointments_controller.rb
@@ -1,37 +1,49 @@
-class AppointmentsController < ApplicationController
+# frozen_string_literal: true
 
+class AppointmentsController < ApplicationController
   # GET: /appointments
-  get "/appointments" do
+  get '/appointments' do
     erb :"/appointments/index.html"
   end
 
   # GET: /appointments/new
-  get "/appointments/new" do
+  get '/appointments/new' do
+    @patients = Patient.all
+
+    if params[:doctor_id]
+      @doctor = Doctor.find(params[:doctor_id])
+    else
+      @doctors = Doctor.all
+    end
+
     erb :"/appointments/new.html"
   end
 
   # POST: /appointments
-  post "/appointments" do
-    redirect "/appointments"
+  post '/appointments' do
+    appointment = Appointment.create(params[:appointment])
+
+    redirect "/appointments/#{appointment.id}"
   end
 
   # GET: /appointments/5
-  get "/appointments/:id" do
+  get '/appointments/:id' do
+    @appointment = Appointment.find(params[:id])
     erb :"/appointments/show.html"
   end
 
   # GET: /appointments/5/edit
-  get "/appointments/:id/edit" do
+  get '/appointments/:id/edit' do
     erb :"/appointments/edit.html"
   end
 
   # PATCH: /appointments/5
-  patch "/appointments/:id" do
-    redirect "/appointments/:id"
+  patch '/appointments/:id' do
+    redirect '/appointments/:id'
   end
 
   # DELETE: /appointments/5/delete
-  delete "/appointments/:id/delete" do
-    redirect "/appointments"
+  delete '/appointments/:id/delete' do
+    redirect '/appointments'
   end
 end
--- 14-sinatra-associations/doctors-surgery/app/models/appointment.rb
+++ 14-sinatra-associations/doctors-surgery/app/models/appointment.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 class Appointment < ActiveRecord::Base
+  belongs_to :doctor
+  belongs_to :patient
 end

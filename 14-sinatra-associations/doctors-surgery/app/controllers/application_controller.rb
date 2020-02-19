b00a190d4b0638f60bc8b5747952707d7b6e2c38
Sam Barker <samuel.e.barker@googlemail.com>
2020-02-19 09:33:18 +0000

    associated sinatra before lecture

---
+++ 14-sinatra-associations/doctors-surgery/app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  get '/' do
+    # binding.pry
+    erb :welcome
+  end
+end

aa05e5dcaf1f62e424c827e3e42b091bfbb94c18
Sam Barker <samuel.e.barker@googlemail.com>
2020-02-21 09:20:22 +0000

    16 before lecture

---
+++ 16-rails-forms-rest/pet-shop/test/controllers/species_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class SpeciesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get species_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get species_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get species_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get species_create_url
+    assert_response :success
+  end
+
+end

ec8d3ae8a4d2cd71de434c6827a5dd383b0dd038
Sam Barker <samuel.e.barker@googlemail.com>
2020-02-27 17:54:12 +0000

    after challenge review:

--- 19-challenge-review/db/seeds.rb
+++ 19-challenge-review/db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Vendor.destroy_all
-Sweet.destroy_all
+# Vendor.destroy_all
+# Sweet.destroy_all
 
 vendors = ["Insomnia Cookies", "Cookies Cream","Carvel","Gregory's Coffee","Duane Park Patisserie","Tribeca Treats"]
 sweets = ["Chocolate Chip Cookie", "Chocolate Chunk Cookie","M&Ms Cookie","White Chocolate Cookie","Brownie","Peanut Butter Icecream Cake", ]

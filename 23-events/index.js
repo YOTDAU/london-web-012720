1a9bcbedd1b0630b9843a7cece2857f634811615
Sam Barker <samuel.e.barker@googlemail.com>
2020-03-10 13:51:30 +0000

    after events

--- 23-events/index.js
+++ 23-events/index.js
@@ -1,6 +1,82 @@
 // FUNCTIONS
 
 // function()
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+const sumAgain = function(num1, num2) {
+  return num1 + num2;
+};
+
 // () =>
 
+const sumArrow = (num1, num2) => {
+  return num1 + num2;
+};
+
+const sumImpliedReturnArrow = (num1, num2) => num1 + num2;
+const double = num => num * 2;
+const three = () => 3;
+
+const array = [1, 2, 34];
+array.map(function(number) {
+  return number * 2;
+});
+array
+  .map(number => number * 2)
+  .map(number => number * 2)
+  .map(number => number * 2);
+
 // EVENT LISTENERS
+
+document.querySelector("a").addEventListener("click", e => {
+  e.target.href = "https://bing.com";
+});
+
+const addNoiseButton = document.querySelector("ul > li:last-child > button");
+const newNoiseInput = document.querySelector("ul > li:last-child > input");
+const noises = document.querySelector("ul");
+
+const createNewNoise = newNoiseText => {
+  const newLi = document.createElement("li");
+  newLi.innerText = newNoiseText;
+  noises.append(newLi);
+};
+
+addNoiseButton.addEventListener("click", () => {
+  createNewNoise(newNoiseInput.value);
+});
+
+const userForm = document.querySelector("form");
+
+const handleFormSubmit = event => {
+  event.preventDefault();
+  const userObject = {
+    firstname: event.target.elements.firstname.value,
+    lastname: event.target.elements.lastname.value,
+    nickname: event.target.elements.nickname.value,
+    middlename: event.target.elements.middlename.value
+  };
+
+  const keys = ["firstname", "lastname", "nickname", "middlename"];
+
+  const otherUserObject = {};
+
+  keys.forEach(key => {
+    otherUserObject[key] = event.target.elements[key].value;
+  });
+  console.log("send this to server", userObject, otherUserObject);
+};
+
+userForm.addEventListener("submit", handleFormSubmit);
+
+document.addEventListener("scroll", () => {
+  if (
+    document.body.scrollHeight - window.innerHeight - window.screenY ===
+    821
+  ) {
+    console.log("reached the bottom");
+  }
+});
